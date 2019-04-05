9e2e7fe827ac7f91299dfb4c444d843ea467d08d
Charles Chigoriwa <cchigoriwa@gmail.com>
2019-04-05 09:54:18 +0200

    Add isAuthenticated() to dhis2 auth interceptor

--- src/main/java/ca/uhn/fhir/jpa/starter/custom/DHIS2AuthInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/custom/DHIS2AuthInterceptor.java
@@ -5,6 +5,7 @@ import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
 import java.util.List;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  *
@@ -14,11 +15,14 @@ public class DHIS2AuthInterceptor extends AuthorizationInterceptor {
 
     @Override
     public List<IAuthRule> buildRuleList(RequestDetails theRequestDetails) {
-        boolean authorized = true;
+        boolean authorized = false;
         String bearertoken = theRequestDetails.getHeader("Authorization");
         if (bearertoken != null) {
             String token = bearertoken.split(" ")[1];
             authorized = checkToken(token);
+        } else {
+            //For already logged in user
+            authorized = SecurityContextHolder.getContext().getAuthentication() != null;
         }
         if (authorized) {
             return new RuleBuilder()
--- src/main/java/ca/uhn/fhir/jpa/starter/custom/SecSecurityConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/custom/SecSecurityConfig.java
@@ -28,7 +28,7 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login","**/fhir/**")
+                .antMatchers("/login","**/fhir/**","**/sadombo/**")
                 .permitAll()
                 .antMatchers("/","/home","/about","/search","/resource","/history-server","/transaction")
                 .authenticated() 
